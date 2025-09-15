7335a1c0033dc6171e1c21152f8e546addcff88d
An Tran <tranquocan012017@gmail.com>
2025-09-15 21:47:42 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import contactRouter from './app/routes/contact.route.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/contacts', contactRouter);
 
 app.get('/', (req, res) => {
   res.json({ message: "Welcome to contact book application." });
---
+++ app/routes/contact.route.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import contactController from '../controllers/contact.controller.js';
+
+const router = express.Router();
+
+router.route('/')
+  .get(contactController.findAll)
+  .post(contactController.create)
+  .delete(contactController.deleteAll);
+
+router.route('/favorite')
+  .get(contactController.findAllFavorite);
+
+router.route('/:id')
+  .get(contactController.findOne)
+  .put(contactController.update)
+  .delete(contactController.delete);
+
+export default router;
