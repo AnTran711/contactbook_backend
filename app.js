137099dbf71780142a42cfc31b175382105a8706
An Tran <tranquocan012017@gmail.com>
2025-09-15 22:24:49 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import contactRouter from './app/routes/contact.route.js';
+import ApiError from './app/api_error.js';
 
 const app = express();
 
@@ -11,6 +12,18 @@ app.use('/api/contacts', contactRouter);
 
 app.get('/', (req, res) => {
   res.json({ message: "Welcome to contact book application." });
+});
+
+// Handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, 'Resource not found'));
+});
+
+// Define error-handling middleware last
+app.use((err, req, res) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || 'Internal Server Error'
+  });
 });
 
 export default app;
